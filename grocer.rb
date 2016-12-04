c74b597bc3166814e69716494c01d3bcb113fc36
entcheva <christina.entcheva@gmail.com>
2016-12-04 17:46:27 -0500

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,15 @@
 def consolidate_cart(cart)
-  # code here
+  hash = {}
+
+  cart.each do |items, hashes|
+    items.each do |item, data|
+      if hash[item].nil?
+        hash[item] = data
+      end
+      hash[item][:count].nil? ? hash[item][:count] = 1 : hash[item][:count] += 1
+    end
+  end
+  hash
 end
 
 def apply_coupons(cart, coupons)
@@ -13,3 +23,17 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+
+
+
+[
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"KALE"    => {:price => 3.0, :clearance => false}}
+]
+
+{
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 2},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1}
+}
